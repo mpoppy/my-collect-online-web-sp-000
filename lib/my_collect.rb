4916ba7cb47890e4623f697126d7ba423da4e6ab
Marc Poppy <poppy.marc@gmail.com>
2019-05-13 02:15:20 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+#write a method that behaves like the real #collect
 
-
+def my_collect(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
